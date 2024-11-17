2de724da0281c90a535b920494a18070320913ff
Muhammad Ubaid <mu0826960@gmail.com>
2024-11-17 23:21:17 +0500

    .

--- section 4 (Fundamentals of NodeJs)/Introduction to FS Module/app.js
+++ section 4 (Fundamentals of NodeJs)/Introduction to FS Module/app.js
@@ -1,16 +1,16 @@
 import fs from 'node:fs/promises'
 
-// const file = fs.readFileSync('./index.html');
+// const file = fs.readFileSync('./file-1.txt);
 // console.log(file.toString());
 
-// fs.readFile('./index.html',(err,data) => {
+// fs.readFile('./file-1.txt',(err,data) => {
 //     const content = data.toString();
 //     console.log(content);
 // });
-// let num = 0;
-// setInterval(() => {
-//     console.log(num++);
-// },[1000])
+let num = 0;
+setInterval(() => {
+    console.log(num++);
+},[1000])
 
 const file = await fs.readFile('./index.html',);
 console.log(file.toString());
